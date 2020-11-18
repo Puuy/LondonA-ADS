864afc27228488431053d0e4d283d87753bc46da
Puuy <39457181+Puuy@users.noreply.github.com>
2020-11-18 17:47:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ const humanizeDuration = require("humanize-duration");
 
 client.on("ready", () => {
   console.log(`Logged in as : ${client.user.username}`);
-  client.user.setActivity(`London A , Ads `);
+  client.user.setActivity(`London A , ADS`);
   client.user.setStatus("idle");
 });
-const owners = ["455108784579149835", "505442667652448277", "732158721257635932", "574728157341679617", "665309785544982531"]
+const owners = ["455108784579149835", "505442667652448277"]
 var prefix = "-"; 
 
 client.on("message", async message => {
